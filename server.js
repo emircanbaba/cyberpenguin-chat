1d5f710209dad756382e53b512b5b5d54e1ec1e2
emircanbaba <emirozer9002@gmail.com>
2026-08-24 19:25:23 +0300

    Update server.js

--- server.js
+++ server.js
@@ -9,14 +9,52 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
   console.log('Bağlandı:', socket.id);
+  let userName = '';
+
   socket.on('user joined', (name) => {
-    socket.broadcast.emit('user joined', name);
+    userName = name;
+    socket.join(`room-${socket.id}`);
+    socket.emit('system message', `🐧 Hoş geldin ${name}. Alpha ile özel sohbettesin.`);
+    // Alpha'ya bildirim
+    socket.broadcast.emit('alpha notification', `${name} bağlandı.`);
   });
-  socket.on('chat message', (data) => {
+
+  // Herkese genel mesaj (sadece Alpha kullanır)
+  socket.on('global message', (data) => {
     io.emit('chat message', data);
   });
+
+  // Özel mesaj (Alpha'dan kullanıcıya veya kullanıcıdan Alpha'ya)
+  socket.on('private message', (data) => {
+    if (data.to === 'Alpha') {
+      // Kullanıcı Alpha'ya yazdı
+      socket.broadcast.emit('alpha private', {
+        from: userName,
+        msg: data.msg
+      });
+      // Kendisine de göster
+      socket.emit('chat message', {
+        from: userName,
+        msg: data.msg,
+        isOwn: true
+      });
+    } else {
+      // Alpha'dan belirli bir kullanıcıya
+      const targetSocket = io.sockets.sockets.get(data.to);
+      if (targetSocket) {
+        targetSocket.emit('chat message', {
+          from: 'Alpha',
+          msg: data.msg,
+          isAlpha: true
+        });
+      }
+    }
+  });
+
   socket.on('disconnect', () => {
-    console.log('Ayrıldı:', socket.id);
+    if (userName) {
+      socket.broadcast.emit('alpha notification', `${userName} ayrıldı.`);
+    }
   });
 });
 
